8c80c659030b75a8c0854be912623accb66e618f
Claudio Bartoli <claudio@barto.li>
2017-02-16 00:00:52 +0100

    fix constructor custom exception

--- src/errors/UnexpectedInputError.ts
+++ src/errors/UnexpectedInputError.ts
@@ -1,6 +1,6 @@
 export class UnexpectedInputError extends Error {
 
-    constructor(public message?: string) {
+    constructor(public message: string) {
         super(message)
         this.name = 'UnexpectedInput'
     }
